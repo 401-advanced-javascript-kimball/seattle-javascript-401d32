4379e44977f90d86920524067607e1215f319c28
JacobKnaack <jacobknck@gmail.com>
2019-10-13 21:21:03 -0700

    adds random student script to assets

---
+++ assets/scripts/getRandomStudent.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const repl = require('repl');
+
+const students = [
+  'Alistair Blake',
+  'Avery Brown',
+  'Peter Carmichael',
+  'Ethan Davis',
+  'Julie Erlemeier',
+  'Sarah Gilliam',
+  'Matthew Heyert',
+  'Adrian Huebner',
+  'Jonathan Andrew Kimball',
+  'Corey J Marchard',
+  'David Zheng',
+];
+
+function getRandomStudent() {
+  const randomIndex = Math.floor(Math.random() * Math.floor(students.length));
+  const student = (students[randomIndex]);
+  students.splice(randomIndex, 1);
+  if (student) {
+    return student;
+  }
+  return 'All Students have been randomly selected :)';
+}
+
+console.log('Random Student function loaded, run with `randomStudent()`')
+repl.start('> ').context.randomStudent = getRandomStudent;
