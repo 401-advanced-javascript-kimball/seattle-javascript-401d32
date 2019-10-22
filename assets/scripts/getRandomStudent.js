be4983423b0f3b9763052782795bb382e4b7099a
JacobKnaack <jacobknck@gmail.com>
2019-10-22 12:34:03 -0700

    updates student list, adds random pairs function

--- assets/scripts/getRandomStudent.js
+++ assets/scripts/getRandomStudent.js
@@ -2,11 +2,10 @@
 
 const repl = require('repl');
 
-const students = [
+let students = [
   'Alistair Blake',
   'Avery Brown',
   'Peter Carmichael',
-  'Ethan Davis',
   'Julie Erlemeier',
   'Sarah Gilliam',
   'Matthew Heyert',
@@ -26,6 +25,38 @@ function getRandomStudent() {
   return 'All Students have been randomly selected :)';
 };
 
-console.log('Random Student function loaded, run with `randomStudent()`');
-repl.start('> ').context.randomStudent = getRandomStudent;
 
+
+function getRandomPairs() {
+  let unused = [
+    'Alistair Blake',
+    'Avery Brown',
+    'Peter Carmichael',
+    'Julie Erlemeier',
+    'Sarah Gilliam',
+    'Matthew Heyert',
+    'Adrian Huebner',
+    'Jonathan Andrew Kimball',
+    'Corey J Marchard',
+    'David Zheng',
+  ]
+
+  const results = [];
+  let student1, student2;
+  while (unused.length) {
+    student1 = unused[Math.floor(Math.random() * Math.floor(unused.length))];
+    student2 = unused[Math.floor(Math.random() * Math.floor(unused.length))];
+
+    if (student1 !== student2) {
+      results.push(`${student1} | ${student2}`);
+      unused.splice(unused.indexOf(student1), 1);
+      unused.splice(unused.indexOf(student2), 1);
+    }
+  }
+  return results;
+}
+
+console.log('Random Student functions loaded \n', 'find random students with: `randomStudent()`\n', 'Make random Pairs with `randomPairs()');
+const r = repl.start('> ');
+r.context.randomStudent = getRandomStudent;
+r.context.randomPairs = getRandomPairs;
