7e3e598f37d97f96868f9104eb31dad4f7f622f3
JacobKnaack <jacobknck@gmail.com>
2019-10-23 12:35:05 -0700

    updates student randomization script

--- assets/scripts/getRandomStudent.js
+++ assets/scripts/getRandomStudent.js
@@ -2,7 +2,7 @@
 
 const repl = require('repl');
 
-let students = [
+const students = [
   'Alistair Blake',
   'Avery Brown',
   'Peter Carmichael',
@@ -14,49 +14,48 @@ let students = [
   'Corey J Marchard',
   'David Zheng',
 ];
+const used = [];
 
 function getRandomStudent() {
-  const randomIndex = Math.floor(Math.random() * Math.floor(students.length));
+  let randomIndex = Math.floor(Math.random() * Math.floor(students.length));
+  while (used.includes(students[randomIndex])) {
+    if (used.length === students.length) {
+      return 'All Students have been randomly selected :)';
+    }
+    randomIndex = Math.floor(Math.random() * Math.floor(students.length));
+  };
   const student = (students[randomIndex]);
-  students.splice(randomIndex, 1);
+  used.push(student);
   if (student) {
     return student;
   }
-  return 'All Students have been randomly selected :)';
+  return 'Random student error!';
 };
 
 
 
 function getRandomPairs() {
-  let unused = [
-    'Alistair Blake',
-    'Avery Brown',
-    'Peter Carmichael',
-    'Julie Erlemeier',
-    'Sarah Gilliam',
-    'Matthew Heyert',
-    'Adrian Huebner',
-    'Jonathan Andrew Kimball',
-    'Corey J Marchard',
-    'David Zheng',
-  ]
-
+  const pool = [...students];
   const results = [];
   let student1, student2;
-  while (unused.length) {
-    student1 = unused[Math.floor(Math.random() * Math.floor(unused.length))];
-    student2 = unused[Math.floor(Math.random() * Math.floor(unused.length))];
+  while (pool.length) {
+    student1 = pool[Math.floor(Math.random() * Math.floor(pool.length))];
+    student2 = pool[Math.floor(Math.random() * Math.floor(pool.length))];
 
     if (student1 !== student2) {
       results.push(`${student1} | ${student2}`);
-      unused.splice(unused.indexOf(student1), 1);
-      unused.splice(unused.indexOf(student2), 1);
+      pool.splice(pool.indexOf(student1), 1);
+      pool.splice(pool.indexOf(student2), 1);
     }
   }
   return results;
 }
 
-console.log('Random Student functions loaded \n', 'find random students with: `randomStudent()`\n', 'Make random Pairs with `randomPairs()');
+console.log(
+  '***** RANDOM STUDENT functions loaded *****\n',
+  'find random students with: `randomStudent()`\n',
+  'Make random Pairs with `randomPairs()'
+);
 const r = repl.start('> ');
 r.context.randomStudent = getRandomStudent;
 r.context.randomPairs = getRandomPairs;
